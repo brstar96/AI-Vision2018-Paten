8cf86fcb7637b8867201b8f2e961f0a49e2a8183
MYEONGGYULEE <brstar96@naver.com>
2019-01-03 14:15:47 +0900

    0103-001 Minor changes
    some comments added (main.py')

--- Test/main.py
+++ Test/main.py
@@ -16,8 +16,8 @@ import numpy as np
 from nsml import DATASET_PATH
 
 import keras
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Activation
+from keras.models import Sequential #NN을 초기화할때 필요한 모듈
+from keras.layers import Dense, Dropout, Flatten, Activation #Dense : NN을 구성하는 Layer를 생성할때 필요한 모듈
 from keras.layers import Conv2D, MaxPooling2D
 from keras.callbacks import ReduceLROnPlateau
 from keras import backend as K
@@ -40,6 +40,9 @@ def bind_model(model):
         # Reference(DB) 개수: 1,127
         # Total (query + reference): 1,322
 
+        #data_loader.py의 test_data_loader 함수에 DATASET_PATH를 인자로 넘겨서 queries_path, references_path 반환받으면 될듯
+
+        #224*224해상도의 RGB 이미지로 전처리수행
         queries, query_img, references, reference_img = preprocess(queries, db)
 
         print('test data load queries {} query_img {} references {} reference_img {}'.
@@ -55,6 +58,7 @@ def bind_model(model):
         reference_img = reference_img.astype('float32')
         reference_img /= 255
 
+        #model로부터 특정
         get_feature_layer = K.function([model.layers[0].input] + [K.learning_phase()], [model.layers[-2].output])
 
         print('inference start')
@@ -77,6 +81,7 @@ def bind_model(model):
         reference_vecs = l2_normalize(reference_vecs)
 
         # Calculate cosine similarity
+        #유클리드 공간에서 두 벡터를 내적한 것(sim_matrix)을 query image와 reference img 간의 유사도로 삼음.
         sim_matrix = np.dot(query_vecs, reference_vecs.T)
 
         retrieval_results = {}
@@ -113,13 +118,12 @@ def preprocess(queries, db):
     img_size = (224, 224)
 
     for img_path in queries:
-        img = cv2.imread(img_path,
-                         1)  # 이미지 파일을 알파 없는 BGR로 읽어옴 (https://opencv-python.readthedocs.io/en/latest/doc/01.imageStart/imageStart.html)
+        img = cv2.imread(img_path, 1)  # 이미지 파일을 알파 없는 BGR로 읽어옴 (https://opencv-python.readthedocs.io/en/latest/doc/01.imageStart/imageStart.html)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # BGR->RGB로 채널순서변경
         img = cv2.resize(img, img_size)  # 224*224 이미지해상도변경
         query_img.append(img)
 
-    for img_path in db:
+    for img_path in db: #상동
         img = cv2.imread(img_path, 1)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         img = cv2.resize(img, img_size)
@@ -149,7 +153,7 @@ if __name__ == '__main__':
     input_shape = (224, 224, 3)  # input image shape
 
     """ Model """
-    model = Sequential()
+    model = Sequential() #Neural Net 초기화
     model.add(Conv2D(32, (3, 3), padding='same', input_shape=input_shape))
     model.add(Activation('relu'))
     model.add(Conv2D(32, (3, 3)))
@@ -183,15 +187,17 @@ if __name__ == '__main__':
 
         """ Initiate RMSprop optimizer """
         opt = keras.optimizers.rmsprop(lr=0.00045, decay=1e-6)
+
+        #학습프로세스 설정
         model.compile(loss='categorical_crossentropy',
                       optimizer=opt,
                       metrics=['accuracy'])
 
         """ Load data """
         print('dataset path', DATASET_PATH)
-        output_path = ['./img_list.pkl', './label_list.pkl']
-        train_dataset_path = DATASET_PATH + '/train/train_data'
-        # train_dataset_path = DATASET_PATH + '/train/train_data'  # Local에서 테스트할경우 사용하는 코드
+        output_path = ['./img_list.pkl', './label_list.pkl'] #train에 사용할 image pickle과 label pickle의 출력경로 설정
+        train_dataset_path = DATASET_PATH + '/train/train_data' #학습 인풋데이터 경로설정
+        # train_dataset_path = '로컬경로'  # Local에서 테스트할경우 사용하는 코드
 
         if nsml.IS_ON_NSML:
             # Caching file
@@ -202,23 +208,24 @@ if __name__ == '__main__':
             train_data_loader(train_dataset_path, input_shape[:2], output_path=output_path)
 
         with open(output_path[0], 'rb') as img_f:
-            img_list = pickle.load(img_f)
+            img_list = pickle.load(img_f) #train에 사용할 refenence img의 pickle을 load(data_loader.py의 train_data_loader참고)
         with open(output_path[1], 'rb') as label_f:
-            label_list = pickle.load(label_f)
+            label_list = pickle.load(label_f) #마찬가지로 label정보만 담긴 pickle을 load
 
-        x_train = np.asarray(img_list)
+        x_train = np.asarray(img_list) #x_train = reference imgs
         labels = np.asarray(label_list)
-        y_train = keras.utils.to_categorical(labels, num_classes=num_classes)
+        y_train = keras.utils.to_categorical(labels, num_classes=num_classes) #y_train = ref img labels
         x_train = x_train.astype('float32')
-        x_train /= 255
+        x_train /= 255 #0.0~1.0 실수범위로 데이터를 축소하기 위해 255 컬러범위로 나눔
         print(len(labels), 'train samples')
 
+        #이 부분은 NSML가이드 다시 확인해볼것
         """ Callback """
         monitor = 'acc'
         reduce_lr = ReduceLROnPlateau(monitor=monitor, patience=3)
 
         """ Training loop """
-        for epoch in range(nb_epoch):
+        for epoch in range(nb_epoch): #default nb_epoch : 5, 다른 파라미터는 args참고
             res = model.fit(x_train, y_train,
                             batch_size=batch_size,
                             initial_epoch=epoch,
