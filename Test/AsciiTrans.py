fff08a42eac891248f703258e96ea24105eeacd5
MYEONGGYULEE <brstar96@naver.com>
2019-01-05 00:31:30 +0900

    0105-001 Minor changes
    try to convert hex to string

---
+++ Test/AsciiTrans.py
@@ -0,0 +1,7 @@
+# 00037ed0ce93ed9826fafad2aef98890
+import codecs
+
+string = '00037ed0ce93ed9826fafad2aef98890'
+decode_hex = codecs.getdecoder("hex_codec")
+# string = decode_hex(string)[]
+print(string.shape)
