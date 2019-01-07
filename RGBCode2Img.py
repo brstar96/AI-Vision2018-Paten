adbf18449d6817b7f4dbae71532866ac1de37fc4
MYEONGGYULEE <brstar96@naver.com>
2019-01-07 17:49:31 +0900

    0107-002 RGB codes to Img
    Done

--- RGBCode2Img.py
+++ RGBCode2Img.py
@@ -6,10 +6,6 @@ txtPath = "./TextMining"
 
 def txt2list(txtPath):
     EmptyNpArr = np.arange(0, 244 * 244 * 3)  # 3채널 244*244이미지어레이 생성
-    # print(len(EmptyNpArr))
-    imgWidth = 224
-    imgHeight = 224
-    imgChannel = 3
 
     with open(txtPath + '/reference_imgs_row.txt') as f:
         txtList = f.readlines()
@@ -21,18 +17,20 @@ def txt2list(txtPath):
     newtxtList_int = [int(n) for n in txtList.split()] #int로 전부 형변환
     EmptyNpArr = np.asarray(newtxtList_int) #List를 numpy arr로 변환
     ImgNpArr = EmptyNpArr.flatten('F')
-    # print(ImgNpArr.reshape(224, 224, 3))
+    TempArr = ImgNpArr.reshape(224, 224, 3)
+    b,g,r = cv2.split(TempArr)
+    rgb_img = cv2.merge([r,g,b])
+    print(rgb_img)
 
     #이미지 출력
-    # cv2.imwrite('query1.jpg', ImgNpArr.reshape(224, 224, 3))
-    cv2.imshow('query1.jpg', ImgNpArr.reshape(224, 224, 3))
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    
+    cv2.imwrite('./query1.jpg', rgb_img)
+
     # 테스트용 txt파일
     # file = open("testfile.txt", "w")
     # for i in range(len(txtList)):
     #     file.write(txtList[i])
     # file.close()
 
+    #Numpy Array test용 txt파일
+    # filw = open()
 txt2list(txtPath)
