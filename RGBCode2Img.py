db38e8acea718bd4112e53140a212da2cb3fe9cb
MYEONGGYULEE <brstar96@naver.com>
2019-01-07 16:45:27 +0900

    0107-001 RGB codes to Img
    Doned code

--- RGBCode2Img.py
+++ RGBCode2Img.py
@@ -2,32 +2,37 @@ import cv2
 import numpy as np
 
 txtPath = "./TextMining"
-imgWidth, imgHeight = 244
-imgChannel = 3
-NewImg = np.zeros(imgWidth,imgHeight,imgChannel) #빈 3채널 244*244이미지어레이 생성
+
 
 def txt2list(txtPath):
+    EmptyNpArr = np.arange(0, 244 * 244 * 3)  # 3채널 244*244이미지어레이 생성
+    # print(len(EmptyNpArr))
+    imgWidth = 224
+    imgHeight = 224
+    imgChannel = 3
+
     with open(txtPath + '/reference_imgs_row.txt') as f:
         txtList = f.readlines()
         txtList = str([x.strip() for x in txtList]) # 공백 제거
-    for char in "_():.[]''":
+    for char in "_():.[]''": #특수문자 제거
         txtList = txtList.replace(char, "")
-        newtxtList = txtList.split(", ")
-        # print(newtxtList)
-    for i, val in enumerate(newtxtList):
-        newtxtList[i] = newtxtList[i].split()
-        print(newtxtList[i])
+        txtList = txtList.replace(",", "")
+        txtList = txtList.replace("  ", " ")
+    newtxtList_int = [int(n) for n in txtList.split()] #int로 전부 형변환
+    EmptyNpArr = np.asarray(newtxtList_int) #List를 numpy arr로 변환
+    ImgNpArr = EmptyNpArr.flatten('F')
+    # print(ImgNpArr.reshape(224, 224, 3))
 
-    for i in imgChannel:
-        for ImgWidth in imgWidth:
-            for imgHeight in imgHeight:
-                # NewImg np array에 newtxtList[2][1]처럼 픽셀값 대입하는부분 작성할것.
-                print(imgHeight)
-
-    #테스트용 txt파일
-    file = open("testfile.txt", "w")
-    for i in range(len(txtList)):
-        file.write(txtList[i])
-    file.close()
+    #이미지 출력
+    # cv2.imwrite('query1.jpg', ImgNpArr.reshape(224, 224, 3))
+    cv2.imshow('query1.jpg', ImgNpArr.reshape(224, 224, 3))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+    # 테스트용 txt파일
+    # file = open("testfile.txt", "w")
+    # for i in range(len(txtList)):
+    #     file.write(txtList[i])
+    # file.close()
 
 txt2list(txtPath)
