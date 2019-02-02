47516aabccd6ace492a6aecbbc33121e626c6a57
MinMolang <pc5432cmsknr@gmail.com>
2019-02-02 23:26:33 +0900

    Update data_loader.py
    augementation 병합

--- data_loader.py
+++ data_loader.py
@@ -9,7 +9,7 @@ import pickle
 
 def train_data_balancing(data_path, img_size,num_classes,nb_epoch):
     class_list = [0] * num_classes  #class마다 이미지가 뽑혔을 때, 증가되는 수를 담기 위한 리스트 [0,0,0,0,0, ... 0] 1383개
-
+    nb_epoch += 1
     for epoch_num in range(nb_epoch):
         label_list = []
         img_list = []
@@ -18,15 +18,18 @@ def train_data_balancing(data_path, img_size,num_classes,nb_epoch):
             if not files:
                 continue
             filenum = class_list[label_idx]
+            #print(str(label_idx) + "class, " +str(filenum)+"filenum, "+ str(class_list[label_idx])+"class_list[label_idx]")
             if(epoch_num<nb_epoch-1):
+                #print("epoch : "+str(epoch_num))
                 filenum += 1  # 매 클래스들마다 재사용됨
                 if (filenum == len(files)):
                     filenum = 0  # 클래스가 가진 max 이미지 개수에 도달하면 초기화
                 class_list[label_idx] = filenum  # 클래스마다 몇번 째 이미지 인덱스가 뽑혀야하는 지 기록
                 label_idx += 1
                 continue
+            #print("i am last epoch")
             ''' 이미지 읽어오는 과정'''
-            filename = files[filenum] #선별된 이미지 이름이 들어가도록 ex) 1. class_list에서 클래스에 맞는 filenum을 찾고 [line20] 2. filenum을 files의 인덱스로
+            filename = files[filenum] #선별된 이미지 이름이 들어가도록 ex) 1. class_list에서 클래스에 맞는 filenum을 찾고 2. filenum을 files의 인덱스로
             img_path = os.path.join(root, filename)
             try:
                 img = cv2.imread(img_path, 1)
@@ -42,8 +45,10 @@ def train_data_balancing(data_path, img_size,num_classes,nb_epoch):
                 filenum = 0 # 클래스가 가진 max 이미지 개수에 도달하면 초기화
             class_list[label_idx] = filenum #클래스마다 몇번 째 이미지 인덱스가 뽑혀야하는 지 기록
             label_idx += 1
-    return img_list,label_list
-
+   # print(str(epoch_num) + "nb_epoch, " + "label_len : " + str(len(label_list)))
+    #print(str(epoch_num) + "nb_epoch, " + "img_len : " + str(len(img_list)))
+    #print(str(epoch_num) + "nb_epoch, " + "class_list_len : " + str(len(class_list)))
+    return img_list, label_list
 def train_data_loader(data_path, img_size, output_path):
     label_list = []
     img_list = []
