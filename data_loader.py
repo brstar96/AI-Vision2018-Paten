1ef2fab98bd69a90a7d39d3f18ed986c462c47aa
MinMolang <pc5432cmsknr@gmail.com>
2019-02-13 16:54:27 +0900

    Update data_loader.py
    주석 추가

--- data_loader.py
+++ data_loader.py
@@ -9,11 +9,23 @@ import pickle
 import numpy as np
 import random
 
+'''
+summary
+1) 작성자 : 박민지
+2) 작성일 : 02-12
+3) 함수설명 : (1) 클래스마다 가진 이미지들을 랜덤하게 중복없이 차례대로 가져오는 함수
+4) 파라미터 : (1) label_list : 이미지들의 라벨을 담는 리스트 
+             (2) img_list : 이미지들을 담는 리스트
+             (3) fork_epoch : fork 했을 때 이전 epoch들을 가져옴 string 타입
+             (4) nb_epoch :  epoch 이 for문에서 돌 때마다 가져옴 int 타입
+5) return img_list와 label_list             
+'''
+
 def train_data_balancing(data_path, img_size,fork_epoch, nb_epoch):
     # nb_epoch은 이전까지 돌아간 epoch 수
     label_list = []
     img_list = []
-    label_idx = 0
+    label_idx = 0 # 라벨을 지정해주기 위한 증가변수 (int)
     fork_epoch = int(fork_epoch)
 
     if fork_epoch == 0:
@@ -23,16 +35,16 @@ def train_data_balancing(data_path, img_size,fork_epoch, nb_epoch):
     for root, dirs, files in os.walk(data_path):
         if not files:
             continue
-        new_epoch = fork_epoch+nb_epoch
+        new_epoch = fork_epoch+nb_epoch # 현재 세션 epoch +  fork 세션 epoch
         #print("seed "+str(new_epoch//len(files)))
-        np.random.seed(new_epoch//len(files))
-        x = np.arange(len(files))
-        np.random.shuffle(x)
-        filenum = new_epoch % len(files) #균일하게
+        np.random.seed(new_epoch//len(files)) #seed 설정 : 돌아간 epoch을 이미지 파일개수로 나누어 몫을 구하면 이미지들이 다 순환되기전까지 seed가 같아짐
+        x = np.arange(len(files)) # 리스트를 이미지 파일 개수만큼 생성
+        np.random.shuffle(x) # 순서를 바꿔줌 
+        filenum = new_epoch % len(files) #균일하게 차례대로 들어가기 위해서는 나머지를 구하면 0 1 2 3 4 5  이렇게 들어감 
         #print("shuffle:"+str(x))
         ''' 이미지 읽어오는 과정'''
         #print("label_idx"+str(label_idx)+"filenum : "+str(filenum))
-        filename = files[x[filenum]]  # 선별된 이미지 이름이 들어가도록 ex) 1. class_list에서 클래스에 맞는 filenum을 찾고 2. filenum을 files의 인덱스로
+        filename = files[x[filenum]]  # 선별된 이미지 이름이 들어가도록 x[] 는 shuffle 된 리스트 그 안에 0 1 2 3 4 5 순환되게 들어감 
         #print("xfilenum"+ str(x[filenum]))
         #print("files" + str(files[x[filenum]]))
         img_path = os.path.join(root, filename)
